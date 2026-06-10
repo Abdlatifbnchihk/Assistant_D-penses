b20106b7b223b53610c97e790593b81e60e98a43
abdellatifbencheikh43-cell <EMAIL_OF_abdellatifbencheikh43-cell>
2026-06-11 01:36:58 +0200

    create the migration tables and test the prosses of queues

---
+++ database/migrations/2026_06_10_160847_create_expenses_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('recu_id')
+                ->constrained('recus')
+                ->cascadeOnDelete();
+
+            $table->string('libelle');
+
+            $table->integer('quantite');
+
+            $table->decimal('prix_unitaire', 10, 2);
+
+            $table->enum('categorie', [
+                'alimentaire',
+                'boissons',
+                'hygiene',
+                'entretien',
+                'autre'
+            ]);
+
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expenses');
+    }
+};
